96e8756ce0c3bcdbab2bbe2c68ac1e0c122e8a3d
julia sheremet <juliajessicasher@gmail.com>
2018-05-16 14:32:26 -0700

    add ngif on buddy list

--- src/app/buddy-list/buddy-list.component.ts
+++ src/app/buddy-list/buddy-list.component.ts
@@ -17,27 +17,22 @@ export class BuddyListComponent implements OnInit {
   userId: string;
   user;
   chatList;
+  b = 0;
+
   constructor(private aimService: AimService, private route: ActivatedRoute, private location: Location, private router: Router) { }
 
   ngOnInit() {
     this.userId = this.route.params['_value']['userId'];
     this.chatList = this.aimService.getBuddiesByUserId(this.userId);
-
-    // $(document).ready(function(){
-    //   $(".buddy-dropDown").click(function(){
-    //     let div = $(".buddyList");
-    //       div.animate({down: '100px'}, "slow");
-    //       div.animate({fontSize: '5em'}, "slow");
-    //   });
-    // });
   }
 
   chatWithBuddy(buddyId: string) {
     window.open(`${location.origin}/chat-display/${this.userId}/${buddyId}`, "chat", "height=300, width=300");
   }
 
-  // seeBuddyList(listOfBuddies: buddies){
-  //   this.buddies = listOfBuddies;
-  // }
+  showBuddies(){
+    this.b = 1;
+  }
+
 
 }
