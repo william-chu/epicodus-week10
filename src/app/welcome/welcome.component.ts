bf13aaf2f88eaefff58e0b939e21cd6344ed8916
William Chu <williamchu51@gmail.com>
2018-05-17 08:13:17 -0700

    prevent sign in on welcome when username is not selected

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AimService } from '../aim.service';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -10,10 +9,16 @@ import { Router } from '@angular/router';
   providers: [AimService]
 })
 export class WelcomeComponent implements OnInit {
+  usernameSet = null;
   currentRoute: string = this.router.url;
   constructor(private aimService: AimService, private router: Router) { }
 
   ngOnInit() {
+
+  }
+
+  usernameSetCheck() {
+    this.usernameSet = true;
   }
 
   setUser(username: string) {
